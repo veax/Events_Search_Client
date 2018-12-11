9d0f82b0f1883da72d30758821528a262aa7b3f8
Julien <julien.paris45@gmail.com>
2018-12-11 13:12:22 +0100

    fix event page direct access

--- src/EventPage.js
+++ src/EventPage.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const EventPage = props => {
-  // let id = props.match.params.event_id
-  const { event }  = props.location.state
-  return (
-    <div className="container">
-      <h5>Event Data</h5>
-      <p>{event.nom}</p>
-      <img src={event.media_1} alt="some text"/>
-    </div>
-  )
+const fetchEvent = async id =>
+{
+	const response = await fetch( `http://localhost:8080/evenement/${ id }` );
+	const data = await response.json();
+
+	return data;
 }
 
-export default EventPage
+export default class EventPage extends Component
+{
+	state = {
+		event: {}
+	}
+
+	constructor( props )
+	{
+		super( props );
+
+		if( props.location.state )
+			this.state = {
+				event: props.location.state.event
+			}
+	}
+
+	componentDidMount()
+	{
+		fetchEvent( this.props.match.params.event_id )
+			.then( event => this.setState( { event } ) )
+	}
+
+	render()
+	{
+		const { event } = this.state;
+
+		return <div className="container">
+			<h5>Event Data</h5>
+			<p>{ event.nom }</p>
+			<img src={ event.media_1 } alt="some text" />
+		</div>
+	}
+}
